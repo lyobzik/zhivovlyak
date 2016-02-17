befb71dc9e12c29f5c6d0e6edd7e14456d2f3ec9
lyobzik <lyobzik@gmail.com>
2016-02-17 23:17:47 +0600

    Install prerequirements.

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+panic() {
+  if [[ ${1+isset} = isset ]]; then
+    echo ${1} 1>&2
+  fi
+  exit ${2:-1}
+}
+
+check_prerequirements() {
+  if [ ! -f /etc/os-release ]; then
+    panic "Cannot detect OS"
+  fi
+
+  source /etc/os-release
+  if [[ "${ID}" != "ubuntu" ]]; then
+    panic "Detected unsupported OS: ${ID} <${PRETTY_NAME}>"
+  fi
+}
+
+check_prerequirements
+
+sudo apt-get update
+sudo apt-get dist-upgrade --yes --force-yes
+sudo apt-get install ansible
