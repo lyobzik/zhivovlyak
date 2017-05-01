3301c0952fa5581e009344cdb6a460c9f15be685
lyobzik <lyobzik@gmail.com>
2017-05-01 22:04:45 +0700

    Deploy all roles from one playbook.

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,23 +30,6 @@ if ! command -v ansible-playbook >/dev/null 2>&1; then
   sudo apt-get install --yes --force-yes -- ansible
 fi
 
-ansible-playbook -i inventory/localhost --extra-vars="roles=ansible" bootstrap.yml -vvvv $@
-ansible-playbook -i inventory/localhost --extra-vars="roles=common" bootstrap.yml -vvvv $@
-ansible-playbook -i inventory/localhost --extra-vars="roles=dev" bootstrap.yml -vvvv $@
-ansible-playbook -i inventory/localhost --extra-vars="roles=scala" bootstrap.yml -vvvv $@
-
-ansible-playbook -i inventory/localhost --extra-vars="roles=git" --extra-vars="enable_sudo=false" git.yml -vvvv $@
-ansible-playbook -i inventory/localhost --extra-vars="roles=ssh" -e "allow_root_login=true" bootstrap.yml -vvvv $@
-ansible-playbook -i inventory/localhost --extra-vars="roles=ssh" bootstrap.yml -vvvv $@
-
-ansible-playbook -i inventory/localhost --extra-vars="roles=zsh" --extra-vars="enable_sudo=false" bootstrap.yml -vvvv $@
-ansible-playbook -i inventory/localhost --extra-vars="roles=docker" --extra-vars="enable_sudo=false" bootstrap.yml -vvvv $@
-ansible-playbook -i inventory/localhost --extra-vars="roles=python" --extra-vars="enable_sudo=false" bootstrap.yml -vvvv $@
-
-ansible-playbook -i inventory/localhost --extra-vars="roles=entertainment" bootstrap.yml -vvvv $@
-ansible-playbook -i inventory/localhost --extra-vars="roles=qtcreator" bootstrap.yml -vvvv $@
-ansible-playbook -i inventory/localhost --extra-vars="roles=local-repo" --extra-vars="enable_sudo=false" bootstrap.yml -vvvv $@
-
-ansible-playbook -i inventory/localhost --extra-vars="roles=mmc" --extra-vars="enable_sudo=false" bootstrap.yml -vvvv $@
+ansible-playbook -i inventory/localhost bootstrap.yml -vvvv $@
 
 popd >/dev/null
